9d3c8dea1e5b8a1eb30da1584ae394d1bcced0c4
vinay singh nainwal <86920648+creatordestro3er@users.noreply.github.com>
2024-07-08 03:54:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://polite-cocada-363f43.netlify.app',
+    origin:'https://lighthearted-syrniki-d4a557.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
