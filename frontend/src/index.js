e9aee1399f6972e0f670de54a4142997df726b3d
vinay singh nainwal <86920648+creatordestro3er@users.noreply.github.com>
2024-07-08 00:37:35 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://messenger-4.onrender.com"
+export const BASE_URL="https://messenger-5.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
