999aee30a017ebd18de96aef2c13974891b2def6
vinay singh nainwal <86920648+creatordestro3er@users.noreply.github.com>
2024-07-08 04:26:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://lighthearted-syrniki-d4a557.netlify.app',
+    origin:'https://merry-brigadeiros-0c9fc6.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
